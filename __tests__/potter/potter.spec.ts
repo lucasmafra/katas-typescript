afd3c5850dc755a37f8fadec33a8ed8a8d8c1b2e
Lucas Mafra <lucas.mafra95@gmail.com>
2018-03-28 11:20:32 -0300

    refactor tests

--- __tests__/potter/potter.spec.ts
+++ __tests__/potter/potter.spec.ts
@@ -1,58 +1,60 @@
-import {Potter} from "../../src/potter/potter"
+import {BookOrder, Books} from "../../src/potter/bookOrder"
 
-describe('Potter', () => {
+describe('BookOrder', () => {
 
-  let potter: Potter
+  let bookOrder: BookOrder
 
-  const item = (book: string, amount: number) => ({ book, amount})
+  const BOOK_UNIT_COST = 8
 
-  const buy = (...items: { book: string, amount: number}[]) => {
+  const item = (Books: Books, amount: number) => ({ Books, amount})
+
+  const order = (...items: { Books: Books, amount: number}[]) => {
     for (const item of items) {
-      potter.buyBook(item.book, item.amount)
+      bookOrder.addItem(item.Books, item.amount)
     }
   }
 
   beforeEach( () => {
-    potter = new Potter()
+    bookOrder = new BookOrder()
   })
 
-  it('should return zero for no books', () => {
-    expect(potter.getTotalCost()).toBe(0)
+  it('should return zero for no book', () => {
+    expect(bookOrder.getTotalCost()).toBe(0)
   })
 
-  it('should return the price of the book itself for one book', () => {
-    buy(item("HP1",1))
-    expect(potter.getTotalCost()).toBe(8)
+  it('should return the price of the Book itself for one book', () => {
+    order(item(Books.HP1,1))
+    expect(bookOrder.getTotalCost()).toBe(BOOK_UNIT_COST)
   })
 
-  it('should return two times the book unit cost for buying two identical books',  () => {
-    buy(item("HP1",2))
-    expect(potter.getTotalCost()).toBe(16)
+  it('should return two times the Book unit cost for buying two identical books',  () => {
+    order(item(Books.HP1,2))
+    expect(bookOrder.getTotalCost()).toBe(2*BOOK_UNIT_COST)
   })
 
-  it('should apply a 5% discount when buying two different books',  () => {
-    buy(item("HP1",1), item("HP2",1))
-    expect(potter.getTotalCost()).toBe(16 - 0.05*16)
+  it('should apply a 5% discount when buying two distinct books',  () => {
+    order(item(Books.HP1,1), item(Books.HP2,1))
+    expect(bookOrder.getTotalCost()).toBe(2*BOOK_UNIT_COST*(1 - 0.05))
   })
 
-  it('should apply a 10% discount when buying three different books', () => {
-    buy(item("HP1",1), item("HP2",1), item("HP3", 1))
-    expect(potter.getTotalCost()).toBe(24 - 0.1*24)
+  it('should apply a 10% discount when buying three distinct books', () => {
+    order(item(Books.HP1,1), item(Books.HP2,1), item(Books.HP3, 1))
+    expect(bookOrder.getTotalCost()).toBe(3*BOOK_UNIT_COST*(1 - 0.1))
   })
 
-  it('should apply a 20% discount when buying four different books', () => {
-    buy(item("HP1",1), item("HP2",1), item("HP3", 1), item("HP4", 1))
-    expect(potter.getTotalCost()).toBe(32 - 0.2*32)
+  it('should apply a 20% discount when buying four distinct books', () => {
+    order(item(Books.HP1,1), item(Books.HP2,1), item(Books.HP3, 1), item(Books.HP4, 1))
+    expect(bookOrder.getTotalCost()).toBe(4*BOOK_UNIT_COST*(1 - 0.2))
   })
 
-  it('should apply a 25% discount when buying five different books', () => {
-    buy(item("HP1",1), item("HP2",1), item("HP3", 1), item("HP4", 1), item("HP5", 1))
-    expect(potter.getTotalCost()).toBe(40 - 0.25*40)
+  it('should apply a 25% discount when buying five distinct books', () => {
+    order(item(Books.HP1,1), item(Books.HP2,1), item(Books.HP3, 1), item(Books.HP4, 1), item(Books.HP5, 1))
+    expect(bookOrder.getTotalCost()).toBe(5*BOOK_UNIT_COST*(1 - 0.25))
   })
 
-  it('should apply discount only for different books', () => {
-    buy(item("HP1",2), item("HP2", 1), item("HP3", 1))
-    expect(potter.getTotalCost()).toBe(32 - 0.1*32)
+  it('should apply discount only for distinct books', () => {
+    order(item(Books.HP1,2), item(Books.HP2, 1), item(Books.HP3, 1))
+    expect(bookOrder.getTotalCost()).toBe(32 - 0.1*32)
   })
 
 })
---
+++ src/potter/bookOrder.ts
@@ -0,0 +1,46 @@
+export class BookOrder {
+
+  private BOOK_UNIT_COST = 8
+  private distinctBooks = new Array<Books>()
+
+  private totalCost: number = 0
+
+  public getTotalCost(): number {
+    const discount = this.getDiscount()
+    return this.totalCost - discount
+  }
+
+  public addItem(newBook: Books, amount: number): void {
+    if (this.isNewBook(newBook)) {
+      this.distinctBooks.push(newBook)
+    }
+    this.totalCost += this.BOOK_UNIT_COST * amount
+  }
+
+  private getDiscount() {
+      return this.BOOK_DISCOUNT_TABLE[this.distinctBooks.length]*this.totalCost
+  }
+
+  private isNewBook(newBook: Books) {
+    return !this.distinctBooks.find(book => book === newBook)
+  }
+
+  private BOOK_DISCOUNT_TABLE: { [key: number]: number } = {
+    // [DISTINCT BOOKS : DISCOUNT]
+        0: 0,
+        1: 0,
+        2: 0.05,
+        3: 0.1,
+        4: 0.2,
+        5: 0.25,
+  }
+
+}
+
+export enum Books {
+  HP1,
+  HP2,
+  HP3,
+  HP4,
+  HP5,
+}
